a2b6db7fbffe6e89352f220f727b69216ea8d24e
BluLupo <hersel1991@gmail.com>
2021-07-08 22:02:22 +0200

    add more information in start command

--- commands/start.py
+++ commands/start.py
@@ -2,5 +2,5 @@ from utils.message import message
 
 def init(update, context):
     bot = context.bot
-    msg = "Ciao io sono {}\nsono stato creato da @squirrelnetwork\nSe vuoi vedere i miei sorgenti digita: /source".format("@"+bot.username)
+    msg = "Ciao io sono {}\nsono stato creato da @squirrelnetwork\nSe vuoi vedere i miei sorgenti digita: /source\nPer funzionare basta digitare /seach terminediricercachevuoi\nESEMPIO: /search android".format("@"+bot.username)
     message(update, context, msg)
